37855d506879867a73a290c6386624b99b233c38
edugreat <anyanwuchinedu687@yahoo.com>
2022-11-23 15:52:02 +0100

    Added the spring boot backend

---
+++ ekademiks/src/main/java/com/edugreat/akademiksresource/AkademiksResourceApplication.java
@@ -0,0 +1,13 @@
+package com.edugreat.akademiksresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkademiksResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkademiksResourceApplication.class, args);
+	}
+
+}
