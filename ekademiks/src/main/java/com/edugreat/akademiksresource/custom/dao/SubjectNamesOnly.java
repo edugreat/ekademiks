f20b210bb6713bf3cf0b5d2ac4934709bc22f0ba
edugreat <anyanwuchinedu687@yahoo.com>
2022-12-01 21:30:43 +0100

    update: adds the interface SubjectNamesOnly
    this interface provides abstraction for jpa projection to retrieve Subject entity object with only name attribute

---
+++ ekademiks/src/main/java/com/edugreat/akademiksresource/custom/dao/SubjectNamesOnly.java
@@ -0,0 +1,13 @@
+package com.edugreat.akademiksresource.custom.dao;
+/*
+ * This interface declares abstract method getName()
+ * to implement jpa's projection and returns the Subject's
+ * name attribute
+ */
+
+public interface SubjectNamesOnly {
+	//abstract method
+	String getName();
+	
+
+}
