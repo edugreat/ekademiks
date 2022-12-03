581710631bbda1b87d69646d994a5c8e6e5bde68
edugreat <anyanwuchinedu687@yahoo.com>
2022-12-03 14:11:14 +0100

    fixes string concatenation on http url

--- front-end/src/app/services/multi.service.ts
+++ front-end/src/app/services/multi.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { Category } from '../model/category';
 import { SubjectDate } from '../util/subject-date';
+import { SubjectName } from '../util/subject-name';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,8 @@ export class MultiService {
   private dateEndpoint = "http://localhost:8080/subject";
  
   private baseUrl:string = "http://localhost:8080/akad/categories";
+
+  private subjectNameEndpoint = this.dateEndpoint+"/date";
 
   constructor(private httpClient: HttpClient) { }
 
@@ -29,9 +32,17 @@ export class MultiService {
    
    //This method returns a dates array from the backend server
   fetchAvailableExamDates(id:number):Observable<SubjectDate[]> {
-    return this.httpClient.get<SubjectDate[]>(this.dateEndpoint+`?id=${id}`);
+    return this.httpClient.get<SubjectDate[]>(`${this.dateEndpoint}?id=${id}`);
   }
 
+  //Method returns Subject array with name attributes
+  fetchSubjectNames(examYear:string):Observable<SubjectName[]>{
+    
+    //routes to the controller class's GET method in the database
+    
+    return this.httpClient.get<SubjectName[]>(`${this.subjectNameEndpoint}?date=${examYear}`);
+
+  }
    
 }
 //declare an interface to fetch the course categories
