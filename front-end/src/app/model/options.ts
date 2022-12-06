6d3f22f94fb134a1e49f60c137291d19433f1278
edugreat <anyanwuchinedu687@yahoo.com>
2022-12-06 18:20:09 +0100

    populates the Options model class

--- front-end/src/app/model/options.ts
+++ front-end/src/app/model/options.ts
@@ -1,2 +1,9 @@
 export class Options {
+
+    constructor(private id?: number,
+                private a?:string,
+                private b?:string,
+                private c?:string,
+                private d?:string,
+                private e?:string){}
 }
