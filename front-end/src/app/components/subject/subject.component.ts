85b7e2f5f46539a336117c249f810c18779a27ea
edugreat <anyanwuchinedu687@yahoo.com>
2022-12-13 16:17:08 +0100

    Injects 'StudentAttemptService' to the 'SubjectComponent'. Makes 'examYear', 'categoryId', 'subjectName' global variables for easy reference and usage.

--- front-end/src/app/components/subject/subject.component.ts
+++ front-end/src/app/components/subject/subject.component.ts
@@ -1,8 +1,10 @@
+import { JsonPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { StudentAttempt } from 'src/app/model/student-attempt';
 import { Subject } from 'src/app/model/subject';
 import { MultiService } from 'src/app/services/multi.service';
+import { StudentAttemptService } from 'src/app/services/student-attempt.service';
 
 @Component({
   selector: 'app-subject',
@@ -12,11 +14,16 @@ import { MultiService } from 'src/app/services/multi.service';
 export class SubjectComponent implements OnInit {
 //declares and initializes empty Subject array
 subjects:Subject[] =[];
+examYear:string ='';
+categoryId:number = 0;
+subjectName:string='';
 
 //declares array to hold students attempts
 attempts:StudentAttempt[] =[];
 
-  constructor(private multiService:MultiService, private router:ActivatedRoute) { }
+  constructor(private multiService:MultiService, 
+    private router:ActivatedRoute,
+    private attemptService:StudentAttemptService) { }
 
   ngOnInit(): void {
 
@@ -30,28 +37,31 @@ attempts:StudentAttempt[] =[];
     //confirm that the required router parameters are present
     const hasAll = (this.router.snapshot.paramMap.has("categoryId"))
     &&
-    (this.router.snapshot.paramMap.has("examName"))
+    (this.router.snapshot.paramMap.has("subjectName"))
     &&(this.router.snapshot.paramMap.has("examYear"));
 
     //implements the method if the condition is true
     if(hasAll){
       
       //get the parameters
-      const examYear = this.router.snapshot.paramMap.get("examYear")!;
-      const examName = this.router.snapshot.paramMap.get("examName")!
-      const categoryId = +this.router.snapshot.paramMap.get("categoryId")!
+     const examYear =  this.examYear = this.router.snapshot.paramMap.get("examYear")!;
+     const subjectName =  this.subjectName = this.router.snapshot.paramMap.get("subjectName")!
+     const categoryId = this.categoryId = +this.router.snapshot.paramMap.get("categoryId")!
       
-      this.multiService.fetchSubjects(examYear, examName, categoryId).subscribe(response=>{
+      this.multiService.fetchSubjects(examYear, subjectName, categoryId).subscribe(response=>{
       this.subjects = response
       })
+      
     }
 
   }
   /**
    * Method that saves student's selected options
-   * @param selectedOption Option selected by the student
+   * @param event The event initiated by the stuent's choosing of options
    */
-  saveAttempt(selectedOption:string){
-//More works to do here
+  saveAttempt(event:Event){
+   const target = event.target as HTMLInputElement;
+    this.attemptService.persistAttempts(target,this.examYear, this.subjectName,this.categoryId);
+
   }
 }
