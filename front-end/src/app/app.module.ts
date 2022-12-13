354d1b04856435f703b7b411ad360da36b23bb75
edugreat <anyanwuchinedu687@yahoo.com>
2022-12-13 16:08:22 +0100

    changes request parameter from 'examName' to 'subjectName' for clarity

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -16,7 +16,7 @@ import { SubjectNameComponent } from './components/subject-name/subject-name.com
 const routes: Routes=[
   {path: 'category/:categoryName/:id', component: DateComponent},
   {path: 'exam/:categoryName/:examYear/:categoryId', component: SubjectNameComponent},
-  {path: "question/:categoryId/:examName/:examYear", component: SubjectComponent}
+  {path: "question/:categoryId/:subjectName/:examYear", component: SubjectComponent}
 ]
 
 @NgModule({
