a1ae04b3a525b22f42a4a10cc8cf3379bb574590
edugreat <anyanwuchinedu687@yahoo.com>
2024-05-12 20:22:51 +0100

    refactored the front-end

--- front-end/README.md
+++ front-end/README.md
@@ -1,6 +1,6 @@
 # FrontEnd
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.2.3.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.2.
 
 ## Development server
 
--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,17 +1,13 @@
 import { Component } from '@angular/core';
-import { SearchService } from './services/search.service';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  found =true;
-  constructor(private searchService:SearchService){
-    this.searchService.found.subscribe(x => this.found = x)
-  }
   title = 'front-end';
-  
-  logo ='assets/logo/edukademiks-logo.png';
 }
